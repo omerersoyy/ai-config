9c8c987037e57dd2703b67e12f3d4334186bd8e8
omerersoyy <omerersoy7@gmail.com>
2025-04-06 22:11:12 +0300

    added basic logger

---
+++ lib/logger.ts
@@ -0,0 +1,5 @@
+export const log = {
+  info: (msg: string) => console.log(`ℹ️  ${msg}`),
+  error: (msg: string) => console.error(`❌ ${msg}`),
+  success: (msg: string) => console.log(`✅ ${msg}`),
+};
